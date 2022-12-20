621afa2a923a2e60158e6d615daa078163baee6e
Sung Jae <gwonseongjae93@gmail.com>
2022-12-20 12:42:30 +0900

    sungjaebabo

--- 소스.c
+++ 소스.c
@@ -1,26 +1,30 @@
 #include<stdio.h>
 #include<time.h>
 #include<stdlib.h>
-int sum;
 int main() {
+	int sum;
+
+	srand(time(NULL));
+	int s;
 	for (int i = 1; i <= 10; i++) {
 		int a = (rand() % 20) + 1;
 		int c = (rand() % 20) + 1;
 		char ran[4] = { '+','-','*','/' };
-		char b = ran[rand() % 4 + 1];
-		int s;
-		switch ('b') {
+		char b = ran[rand() % 4];
+		switch (b) {
 			case '+': sum = a + c; break;
 			case '-': sum = a - c; break;
 			case '*': sum = a * c; break;
 			case '/': sum = a / c; break;
+			default: break;
 		}
 		printf("[%d/10]%d %c %d =",i,a,b,c);
-		scanf_s("%d", s);
+		scanf_s("%d",&s);
+
 		if (sum == s) {
 			printf("Correct!\n");
 		}
-		else if (sum == s) {
+		else{
 			printf("Wrong! Answer is %d\n", sum);
 		}
 	}
